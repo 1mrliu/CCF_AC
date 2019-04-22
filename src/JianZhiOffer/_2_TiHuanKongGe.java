6b57f1e16b56d1fd7b5e6f0fd2840956eb8d980f
liudong <ld815626@hotmail.com>
2019-04-22 16:11:10 +0800

    修改替换空格的代码

--- src/JianZhiOffer/_2_TiHuanKongGe.java
+++ src/JianZhiOffer/_2_TiHuanKongGe.java
@@ -7,7 +7,7 @@ package JianZhiOffer;
  *  请实现一个函数，将一个字符串中的每个空格替换成“%20”。
  *  例如，当字符串为We Are Happy.则经过替换之后的字符串为We%20Are%20Happy。
  */
-public class TiHuanKongGe {
+public class _2_TiHuanKongGe {
     public String replaceSpace(StringBuffer str) {
         // 自带函数的方法一
         // return str.toString().replaceAll(" ","%20");
