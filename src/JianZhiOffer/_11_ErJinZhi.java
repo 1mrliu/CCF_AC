363cff6b9be5a3b95ea0721cf7db5c4cf38dc4ca
liudong <ld815626@hotmail.com>
2019-04-28 10:02:35 +0800

    二进制数的计算

---
+++ src/JianZhiOffer/_11_ErJinZhi.java
@@ -0,0 +1,28 @@
+package JianZhiOffer;
+
+/**
+ * 二进制数中1的个数
+ * 输入一个整数，输出该数二进制表示中1的个数。其中负数用补码表示。
+ */
+public class _11_ErJinZhi {
+    public int NumberOf1(int n) {
+        // 转化为二进制的函数
+        if(n==1) return 1;
+        String s = Integer.toBinaryString(n);
+        char[] array = s.toCharArray();
+        int count = 0;
+        for (char x:array) {
+            if (Character.getNumericValue(x)  == 1){
+                count++;
+            }
+        }
+        return count;
+    }
+
+    public static void main(String[] args){
+        _11_ErJinZhi ss = new _11_ErJinZhi();
+        int n = 10;
+        System.out.print(ss.NumberOf1(n));
+
+    }
+}
