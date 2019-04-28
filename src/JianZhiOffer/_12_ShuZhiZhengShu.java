a71bff3ba8e7f95eb3fba264724cfd8ef496b8f3
liudong <ld815626@hotmail.com>
2019-04-28 10:01:59 +0800

    数值整数的次方计算

---
+++ src/JianZhiOffer/_12_ShuZhiZhengShu.java
@@ -0,0 +1,26 @@
+package JianZhiOffer;
+
+/**
+ * 数值的整数次方
+ *给定一个double类型的浮点数base和int类型的整数exponent。求base的exponent次方。
+ */
+
+public class _12_ShuZhiZhengShu {
+    public double Power(double base, int exponent) {
+        // exponent 小于0的情况
+        boolean flag = true;
+        if (exponent < 0){
+            flag = false;
+            exponent = Math.abs(exponent);
+        }
+        double result = 1.0;
+        for (int i = 0; i < exponent; i++) {
+            result *= base;
+        }
+        if (flag == true){
+            return result;
+        }else{
+            return 1/result;
+        }
+    }
+}
