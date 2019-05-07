c2d2093f9506e94d07c6e5e6ce3476f371547f6e
liudong <ld815626@hotmail.com>
2019-05-07 13:24:24 +0800

    最小的K个数

---
+++ src/JianZhiOffer/_29_TopKMinNumber.java
@@ -0,0 +1,49 @@
+package JianZhiOffer;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+/**
+ * 最小的K个数
+ * 输入n个整数，找出其中最小的K个数。例如输入4,5,1,6,2,7,3,8这8个数字，则最小的4个数字是1,2,3,4,。
+ */
+
+public class _29_TopKMinNumber {
+    public ArrayList<Integer> GetLeastNumbers_Solution(int [] input, int k) {
+
+
+        ArrayList<Integer> result = new ArrayList<Integer>();
+        if (input.length<k) return result;
+        Arrays.sort(input);
+        for (int i = 0; i < k; i++) {
+            result.add(input[i]);
+        }
+        return result;
+
+        /**
+         * 使用堆进行处理 O(nlogK)
+         * ArrayList<Integer> res = new ArrayList<>();
+         *         if (input == null || k <= 0 || k > input.length) {
+         *             return res;
+         *         }
+         *         Queue<Integer> queue = new PriorityQueue<>(k, Collections.reverseOrder());
+         *
+         *         for (int i = 0; i < input.length; i++) {
+         *
+         *             if (queue.size() < k) {
+         *                 queue.add(input[i]);
+         *             } else {
+         *                 if (input[i] < queue.peek()) {
+         *                     queue.remove();
+         *                     queue.add(input[i]);
+         *                 }
+         *             }
+         *         }
+         *         while (!queue.isEmpty()) {
+         *             res.add(queue.remove());
+         *         }
+         *         return res;
+         */
+
+    }
+}
