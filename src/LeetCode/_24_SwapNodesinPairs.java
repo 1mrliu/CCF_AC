1a489a3804e4ffb48369ac0a3c53f30c028d6e84
liudong <ld815626@hotmail.com>
2019-08-31 13:39:28 +0800

    add  solution2 deal this problem

--- src/LeetCode/_24_SwapNodesinPairs.java
+++ src/LeetCode/_24_SwapNodesinPairs.java
@@ -25,4 +25,20 @@ public class _24_SwapNodesinPairs {
         n.next = head;
         return n;
     }
+
+    public ListNode swapPairsI(ListNode head){
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+        ListNode point = dummy;
+        while (point.next != null && point.next.next != null){
+            ListNode swap1 = point.next;
+            ListNode swap2 = point.next.next;
+
+            point.next = swap2;
+            swap1.next = swap2.next;
+            swap2.next = swap1;
+            point = swap1;
+        }
+        return dummy.next;
+    }
 }
