1f6444b67e22a9008199e22dcce823c5cec68c28
liudong <ld815626@hotmail.com>
2019-09-06 21:02:25 +0800

    AC 48 RotateImage

---
+++ src/LeetCode/_48_RotateImage.java
@@ -0,0 +1,37 @@
+package LeetCode;
+
+public class _48_RotateImage {
+    public void rotate(int[][] matrix) {
+        int row = matrix.length;
+        int col = matrix[0].length;
+        for (int i = 0; i < row; i++) {
+            for (int j = i; j < col; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length/2; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[i][matrix.length-1-j];
+                matrix[i][matrix.length-1-j] = temp;
+            }
+        }
+    }
+
+    public static void main(String[] args){
+        int[][] matrix = {{1,2,3},{4,5,6},{7,8,9}};
+        _48_RotateImage re = new _48_RotateImage();
+        re.rotate(matrix);
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                System.out.print(matrix[i][j]);
+            }
+            System.out.print('\n');
+        }
+
+    }
+
+}
